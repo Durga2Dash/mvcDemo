3573a19db9f0706c97879ab6f7712a3e97a131e7
Durga2Dash <durgamadhabdash_kumar@srmuniv.edu.in>
2019-01-01 15:09:16 +0530

    Update DbUtil.java

--- src/main/java/com/mindtree/util/DbUtil.java
+++ src/main/java/com/mindtree/util/DbUtil.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DbUtil {
 
 	public static final String CLASSNAME = "com.mysql.jdbc.Driver";
-	public static final String URL = "jdbc:mysql://172.17.0.2:3306/spring_user_db?allowPublicKeyRetrieval=true&useSSL=FALSE";
+	public static final String URL = "jdbc:mysql://172.17.0.3:3306/spring_user_db?allowPublicKeyRetrieval=true&useSSL=FALSE";
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "Welcome123";
+	public static final String PASSWORD = "password";
 
 	public static Connection getConnection() {
 		Connection conn = null;
